a13160e961ff8e9954b6f86e1366995b0b586996
Vladimir <komarov2142@gmail.com>
2020-01-31 15:30:50 +1000

    update

--- src/js/index.js
+++ src/js/index.js
@@ -2,12 +2,12 @@
 import "normalize.css";
 import "../scss/main.scss";
 import "animate.css/animate.min.css";
-import "../css/vendors/bootstrap.css";
+// import "../css/vendors/bootstrap.css";
 
 //js
 import "./header";
 import WOW from "wow.js/dist/wow.min.js";
-import "./bootstrap.js";
+// import "./bootstrap.js";
 
 new WOW().init();
 
--- webpack.dev.js
+++ webpack.dev.js
@@ -82,7 +82,7 @@ const config = {
         ]
       },
       {
-        test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
+        test: /\.(woff(2)?|ttf|eot|otf)(\?v=\d+\.\d+\.\d+)?$/,
         use: [
           {
             loader: 'file-loader',
