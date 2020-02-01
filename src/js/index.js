e070205353b29782a7682b709deb44dc5dff98a0
Vladimir <komarov2142@gmail.com>
2020-02-01 11:16:25 +1000

    basic carousel

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,23 @@
 //styles
 import "../css/vendors/bootstrap.css";
 import "normalize.css";
+import 'owl.carousel/dist/assets/owl.carousel.min.css'
 import "../scss/main.scss";
 import "animate.css/animate.min.css";
 
 //js
 import "./header";
 import WOW from "wow.js/dist/wow.min.js";
-// import "./bootstrap.js";
+import 'owl.carousel/dist/owl.carousel.min.js';
 
 new WOW().init();
 
-// //init bootstrap's carousel
-// $('.carousel').carousel({
-//     interval: 2000
-// });
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        items: 1,
+        nav: true,
+        navContainerClass: "owl-nav carousel__pagination",
+        navClass: ["carousel__arrow carousel__arrow_left", "carousel__arrow carousel__arrow_right"],
+        navText: ""
+    });
+});
